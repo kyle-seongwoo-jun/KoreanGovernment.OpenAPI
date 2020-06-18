6c037bddc96a7d8c3d027ba1b81262ce2a56089d
Kyle Seongwoo Jun <njk230@gmail.com>
2020-06-18 22:19:24 +0900

    Initialize repo

---
+++ KoreanGovernment.OpenAPI.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace KoreanGovernment.OpenAPI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
